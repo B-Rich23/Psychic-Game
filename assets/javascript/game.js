ce705ebcc026404853039c88dcae2329ab84dd9a
Brandan Richardson <brandanrichardson@brandans-mbp.attlocal.net>
2018-01-18 21:11:26 -0800

    update 1/18/18 9:11PM

---
+++ assets/javascript/game.js
@@ -0,0 +1,3 @@
+var secretLetter = ["a", "b", "c", "d", "e", "f","g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+var userInput = prompt("What's your guess?")
