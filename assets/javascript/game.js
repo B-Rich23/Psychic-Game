b09f3f79be0238a540e223e0ec2ae42fc27fa4fa
Brandan Richardson <brandanrichardson@brandans-mbp.attlocal.net>
2018-01-18 22:51:15 -0800

    update 1/18/18 10:51PM

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,22 @@
-var secretLetter = ["a", "b", "c", "d", "e", "f","g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var secretLetter = ["a", "b", "c", "d", "e", "f","g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-var userInput = prompt("What's your guess?")
+var computerChoice = secretLetter[Math.floor(Math.random() * secretLetter.length)];
+
+var userGuess ; 
+
+var result;
+
+document.onkeyup = function(event) {
+	userInput = event.key
+};
+
+while(userInput !== secretLetter) {
+	
+}
+
+if (userInput === secretLetter) {
+
+}
+else (userInput !== secretLetter) {
+
+}
